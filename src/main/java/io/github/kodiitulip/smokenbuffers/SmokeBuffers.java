74ce05ccc2c487f5698c46af50b10089badada8b
kodiitulip <kodii.tulip@gmail.com>
2025-11-26 17:02:52 -0300

    fix: idea warnings

--- src/main/java/io/github/kodiitulip/smokenbuffers/SmokeBuffers.java
+++ src/main/java/io/github/kodiitulip/smokenbuffers/SmokeBuffers.java
@@ -9,14 +9,10 @@ import org.slf4j.Logger;
 import com.mojang.logging.LogUtils;
 
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
 @Mod(SmokeBuffers.MODID)
 public class SmokeBuffers {
--- src/main/java/io/github/kodiitulip/smokenbuffers/data/SmokeStackShapeEnum.java
+++ src/main/java/io/github/kodiitulip/smokenbuffers/data/SmokeStackShapeEnum.java
@@ -2,12 +2,13 @@ package io.github.kodiitulip.smokenbuffers.data;
 
 import net.createmod.catnip.lang.Lang;
 import net.minecraft.util.StringRepresentable;
+import org.jetbrains.annotations.NotNull;
 
 public enum SmokeStackShapeEnum implements StringRepresentable {
     SINGLE, DOUBLE, CONNECTED;
 
     @Override
-    public String getSerializedName() {
+    public @NotNull String getSerializedName() {
         return Lang.asId(name());
     }
 }
